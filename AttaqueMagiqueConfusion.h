5dd714c101d78a395ea6fa5e7783be077b2ebccd
Dz1x <36559220+Dz1x@users.noreply.github.com>
2019-06-01 00:21:12 -0400

    Terminé

--- AttaqueMagiqueConfusion.h
+++ AttaqueMagiqueConfusion.h
@@ -1,1 +1,41 @@
 
+#include "AttaqueMagique.h"
+
+#ifndef ATTAQUEMAGIQUECONFUSION_H
+#define ATTAQUEMAGIQUECONFUSION_H
+
+
+class AttaqueMagiqueConfusion : public AttaqueMagique {
+	
+
+	AttaqueMagiqueConfusion();
+	AttaqueMagiqueConfusion(const int&duree);
+	~AttaqueMagiqueConfusion();
+	
+	virtual void appliquerAttaque(Creature&creature);
+
+	bool estFini();
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
