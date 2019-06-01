f0077799da488013c6595d563124dbe388117a2a
Dz1x <36559220+Dz1x@users.noreply.github.com>
2019-06-01 00:27:26 -0400

    Terminé

--- AttaqueMagiquePoison.h
+++ AttaqueMagiquePoison.h
@@ -1,1 +1,27 @@
 
+#include "AttaqueMagique.h"
+
+#ifndef   ATTAQUEMAGIQUEPOISON_H
+#define   ATTAQUEMAGIQUEPOISON_H
+
+class AttaqueMagiquePoison : public AttaqueMagique {
+
+public:
+	AttaqueMagiquePoison();
+	AttaqueMagiquePoison(const int&duree);
+	~AttaqueMagiquePoison();
+	virtual void appliquerAttaque(Creature&creature);
+	bool estFini();
+
+
+
+
+};
+
+
+
+
+
+
+
+#endif
