e5a98cbac542da0df5735fe58bf1d860218a4ed1
Dz1x <36559220+Dz1x@users.noreply.github.com>
2019-06-01 00:20:30 -0400

    faut revoir le truc de obtenir type attaque dans le main

--- AttaqueMagique.cpp
+++ AttaqueMagique.cpp
@@ -1,1 +1,47 @@
 
+#include "AttaqueMagique.h"
+
+ostream & operator<<(ostream & os, const AttaqueMagique & attaqueMagique)
+{
+	os << "attaque de type :" << attaqueMagique.obtenirTypeAttaque() << " qui a une duree de " << attaqueMagique.obtenirDuree() << endl;
+	return os;
+}
+
+AttaqueMagique::AttaqueMagique():duree_(0)
+{
+
+}
+
+AttaqueMagique::AttaqueMagique(const int & duree):duree_(duree)
+{
+}
+
+AttaqueMagique::~AttaqueMagique()
+{
+}
+
+int AttaqueMagique::obtenirDuree() const
+{
+	return duree_;
+}
+
+void AttaqueMagique::modifierDuree(const int & duree)
+{
+	duree_ = duree;
+}
+
+string AttaqueMagique::obtenirTypeAttaque() const
+{
+	return typeid(*this).name();
+}
+
+void AttaqueMagique::appliquerAttaquer(Creature & creature)
+{
+}
+
+
+
+bool AttaqueMagique::estfini()
+{
+	return true;
+}
