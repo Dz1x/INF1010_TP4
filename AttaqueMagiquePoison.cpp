3ef0c5810016274f1531d2a6e757f23e221a31f4
Dz1x <36559220+Dz1x@users.noreply.github.com>
2019-06-01 00:27:57 -0400

    Terminé

--- AttaqueMagiquePoison.cpp
+++ AttaqueMagiquePoison.cpp
@@ -1,1 +1,39 @@
 
+#include "AttaqueMagiquePoison.h"
+
+AttaqueMagiquePoison::AttaqueMagiquePoison(): AttaqueMagique(2)
+{
+
+}
+
+AttaqueMagiquePoison::AttaqueMagiquePoison(const int & duree): AttaqueMagique(duree)
+{
+}
+
+AttaqueMagiquePoison::~AttaqueMagiquePoison()
+{
+}
+
+void AttaqueMagiquePoison::appliquerAttaque(Creature & creature)
+{
+	
+	if (obtenirDuree() > 0 && creature.obtenirEnergie() >= 5)
+		{
+			unsigned int energie = creature.obtenirEnergie();
+			if (rand() % 3 == 0)
+			{
+				creature.modifierPointDeVie(energie - 2);
+			}
+			modifierDuree(obtenirDuree()-1);
+		}
+	
+}
+
+bool AttaqueMagiquePoison::estFini()
+{
+	if (obtenirDuree() == 0) {
+		return true;
+	}
+	return false;
+
+}
