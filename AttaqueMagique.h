b6df9a65802ebf83491a93f63fd73ca9dfc08ac8
Dz1x <36559220+Dz1x@users.noreply.github.com>
2019-06-01 00:19:27 -0400

    Terminé

--- AttaqueMagique.h
+++ AttaqueMagique.h
@@ -1,1 +1,40 @@
 
+#include "Creature.h"
+#include <string>
+#include <iostream>
+#ifndef ATTAQUEMAGIQUE_H
+#define ATTAQUEMAGIQUE_H
+
+using namespace std;
+class AttaqueMagique {
+
+public:
+
+	AttaqueMagique();
+	AttaqueMagique(const int&duree);
+	~AttaqueMagique();
+
+	int obtenirDuree() const;
+	void modifierDuree(const int&duree);
+    string obtenirTypeAttaque() const;
+	virtual void appliquerAttaquer(Creature&creature);
+	bool estfini();
+
+	friend ostream&operator<<(ostream&os,const AttaqueMagique&attaqueMagique);
+
+	
+
+
+
+private:
+	unsigned int duree_;
+
+
+
+};
+
+
+
+
+
+#endif
