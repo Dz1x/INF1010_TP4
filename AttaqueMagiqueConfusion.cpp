889874499ffedddeb1a46c25ef68d87cef6bcdd8
Dz1x <36559220+Dz1x@users.noreply.github.com>
2019-06-01 00:25:08 -0400

    Terminé

--- AttaqueMagiqueConfusion.cpp
+++ AttaqueMagiqueConfusion.cpp
@@ -1,1 +1,41 @@
 
+#include "AttaqueMagiqueConfusion.h"
+
+
+AttaqueMagiqueConfusion::AttaqueMagiqueConfusion(): AttaqueMagique(2){
+	
+
+}
+
+AttaqueMagiqueConfusion::AttaqueMagiqueConfusion(const int&duree) : AttaqueMagique(duree) {
+
+}
+
+AttaqueMagiqueConfusion::~AttaqueMagiqueConfusion()
+{
+}
+
+
+
+
+void AttaqueMagiqueConfusion::appliquerAttaque(Creature & creature)
+{
+	if (obtenirDuree() > 0 && creature.obtenirPointDeVie() >= 5)
+	{
+		unsigned int pointDeVie = creature.obtenirPointDeVie();
+		if (rand() % 3 == 0)
+		{
+			creature.modifierPointDeVie(pointDeVie - 2);
+		}
+		modifierDuree(obtenirDuree()-1);
+	}
+}
+
+bool AttaqueMagiqueConfusion::estFini()
+{
+	if (obtenirDuree() == 0) {
+		return false;
+	}
+	return true;
+}
+
